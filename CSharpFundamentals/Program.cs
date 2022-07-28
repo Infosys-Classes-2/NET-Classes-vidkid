636da12a4e059abe6f846f484ff4a27276d752f0
vidkid <dhirajsah22003@gmail.com>
2022-07-28 17:27:08 +0545

    area of circle

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -9,7 +9,7 @@
 // var firstname = yourfullname.Substring(0, len);
 // Console.WriteLine($"Hello, {firstname}");
 
-// class 1
+// class work 1
  
 //  Console.WriteLine("enter your name");
 //  string name =  Console.ReadLine();
@@ -18,10 +18,20 @@
  
 
 
-//  class 2
+//  class work 2
 
 // Console.WriteLine("enter meter value: ");
 // var length = Console.ReadLine();
 // var lengthnumber = double.Parse(length);
 // var feets = lengthnumber * 3.288;
 // Console.WriteLine($"{lengthnumber} is {feets}");
+
+
+//class work 3
+
+Console.WriteLine("Enter the Radius: ");
+var r = Console.ReadLine();
+var radius = Convert.ToInt32(r);
+var p = 3.1428;
+var area = p *  radius * radius;
+Console.WriteLine($"The area of circle is: {area}");
