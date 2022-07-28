6730a2a4ce64b5c4fc78e76af4b33de51ade7c15
vidkid <dhirajsah22003@gmail.com>
2022-07-29 00:14:46 +0545

    coverted days to year, month, week and days done

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -27,11 +27,24 @@
 // Console.WriteLine($"{lengthnumber} is {feets}");
 
 
-//class work 3
 
-Console.WriteLine("Enter the Radius: ");
-var r = Console.ReadLine();
-var radius = Convert.ToInt32(r);
-var p = 3.1428;
-var area = p *  radius * radius;
-Console.WriteLine($"The area of circle is: {area}");
+// //class work 3
+
+// Console.WriteLine("Enter the Radius: ");
+// var r = Console.ReadLine();
+// var radius = Convert.ToInt32(r);
+// var p = 3.1428;
+// var area = p *  radius * radius;
+// Console.WriteLine($"The area of circle is: {area}");
+
+
+
+// class work 4
+
+Console.WriteLine("Enter days: ");
+var d = Console.ReadLine();
+var days = Convert.ToInt32(d);
+var year = days/365;
+var month = days/12;
+var week = days/7;
+Console.WriteLine($"The year is {year}, month is {month}, week is {week} and days is {days}");
