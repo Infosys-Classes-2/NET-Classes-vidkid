2eb1576ef2aa485e69ef15de1916b01ad207a186
vidkid <dhirajsah22003@gmail.com>
2022-07-28 09:54:42 +0545

    capitalized

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,6 +1,17 @@
 ﻿using System;
 
-Console.WriteLine("your name: ");
-string name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}");
-Console.ReadLine();
+// Console.WriteLine("your name: ");
+// string name = Console.ReadLine();
+// Console.WriteLine($"Hello, {name}");
+// Console.WriteLine("yourfullname");
+// var yourfullname = Console.ReadLine();
+// var len = yourfullname.IndexOf(" ");
+// var firstname = yourfullname.Substring(0, len);
+// Console.WriteLine($"Hello, {firstname}");
+
+// class 1
+ string name;
+ Console.WriteLine("enter your name");
+ Console.ReadLine();
+ Console.WriteLine(name.ToUpper());
+ 
