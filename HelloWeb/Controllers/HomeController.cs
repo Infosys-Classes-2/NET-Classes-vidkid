2bfe5dd97bd60fe285504f7e2aa8a7c7cb2104cd
vidkid <dhirajsah22003@gmail.com>
2022-08-24 02:07:17 +0545

    Completed webarray assignment

--- HelloWeb/Controllers/HomeController.cs
+++ HelloWeb/Controllers/HomeController.cs
@@ -1,18 +1,9 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using HelloWeb.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HelloWeb.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return View();
@@ -22,14 +13,9 @@ public class HomeController : Controller
     {
         return View();
     }
+
     public string Test()
     {
-        return "hello!!";
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return "Hello there!!!";
     }
 }
---
+++ HelloWeb/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+        public IActionResult Print(int limit)
+        {
+            return View(limit);
+        }
+        public IActionResult Table5()
+        {
+            return View();
+        }
+        public IActionResult Multiply5(int multiple)
+        {
+            return View(multiple);
+        }
+        public IActionResult Integer()
+        {
+            return View();
+        }
+    }
+}
