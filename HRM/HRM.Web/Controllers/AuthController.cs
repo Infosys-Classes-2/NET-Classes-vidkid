ae085e1414ca3bc598015bc35f6eef67539e7cd8
Dhiraj Shah <dhirajsah22003@gmail.com>
2022-09-14 15:05:15 +0545

    assigned roles

--- HRM/HRM.Web/Controllers/AuthController.cs
+++ HRM/HRM.Web/Controllers/AuthController.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using HRM.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HRM.Web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<IdentityUser> userManager;
 
-        public AuthController(RoleManager<IdentityRole> rolemanager, UserManager<IdentityUser> userManager)
+        public AuthController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
         {
-            this.roleManager = rolemanager;
+            this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         public IActionResult Roles()
         {
@@ -29,7 +33,30 @@ namespace HRM.Web.Controllers
         }
         public IActionResult AssignRole()
         {
-            return View();
+            var users = userManager.Users.ToList();
+            var roles = roleManager.Roles.ToList();
+
+            UserRoleViewModel userRoleViewModel = new()
+            {
+                Users = users.Select(x => new SelectListItem { Text = x.UserName, Value = x.Id }),
+                Roles = roles.Select(x => new RoleItem { Value = x.Name, IsSelected = false }).ToList()
+            };
+            return View(userRoleViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(UserRoleViewModel userRoleViewModel)
+        {
+            var user = await userManager.FindByIdAsync(userRoleViewModel.UserId);
+            var rolesSelected = userRoleViewModel.Roles
+                .Where(x => x.IsSelected)
+                .Select(x => x.Value);
+
+            foreach (var role in rolesSelected)
+            {
+                await userManager.AddToRoleAsync(user, role);
+            }
+
+            return RedirectToAction(nameof(AssignRole));
         }
     }
 }
---
+++ HRM/HRM.Web/ViewModels/UserRoleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using NuGet.Protocol.Core.Types;
+
+namespace HRM.Web.ViewModels
+{
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; }
+
+        public IEnumerable<SelectListItem> Users { get; set; }
+        public List<RoleItem> Roles { get; set; }
+    }
+    public class RoleItem 
+    { 
+        public string Value { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+}
